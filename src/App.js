8389f1ee9ee0a65517c81371089cd32b2c82b326
John <jnhernandez2.0@gmail.com>
2019-09-29 21:45:49 -0500

    Second push to new repository - I wanted to push up again before I started to learn about STATE.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
     <div className="App">
       <h1>Hi, I'm a REACT app!</h1>
       <p>This is really working</p>
-      <Person />
+      <Person name="John" age="30"/>
+      <Person name="Erica" age="28"/>
+      <Person name="Adom" age="05">Loves to climb.</Person>
+      <Person name="Ira" age="00"/>
     </div>
   );
 
